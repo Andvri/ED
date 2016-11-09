481a95c85f0e8e8659461ecad99e5c1dc3b4fdfb
JMrtz16 <javier.martinez@unet.edu.ve>
2016-11-08 20:59:54 -0400

    Estructuras que faltan

---
+++ Pilas/Pilas.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Nodo.h"
+
+
+/*
+Tipo de dato Dato que se almacena en la pila
+Operaciones
+CrearPila Inicia la pila.
+Insertar (push) Pone un dato en la pila.
+Quitar (pop) Retira (saca) un dato de la pila.
+Pilavacía Comprobar si la pila no tiene elementos.
+Pilallena Comprobar si la pila está llena de elementos.
+Limpiar pila Quita todos sus elementos y dejar la pila vacía.
+CimaPila Obtiene el elemento cima de la pila.
+Tamaño de la pila Número de elementos máximo que puede contener la pila.
+
+
+*/
+
+
+class Pila{
+};
+
+
+class PilaC{
+	
+};
+
+class PilaD{
+	
+};
